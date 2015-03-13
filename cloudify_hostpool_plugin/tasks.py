a246bce5e438efaa9d260a0c386e01bcbda8562d
Anna Szpoton <anna.szpoton@codilime.com>
2015-03-13 16:30:54 +0100

    CFY-2295 Add password usage

--- cloudify_hostpool_plugin/tasks.py
+++ cloudify_hostpool_plugin/tasks.py
@@ -15,6 +15,7 @@
 import httplib
 import os
 import requests
+import stat
 
 from cloudify import ctx
 from cloudify.exceptions import NonRecoverableError
@@ -23,38 +24,46 @@ from cloudify.decorators import operation
 
 @operation
 def acquire(service_url, **kwargs):
-    ctx.logger.info("Acquire host")
+    ctx.logger.info('Acquire host')
     response = requests.post('{0}/hosts'.format(service_url))
     host = response.json()
-    ctx.logger.info("Response received: {0}".format(str(host)))
+    ctx.logger.debug('Response received: {0}'.format(str(host)))
     if response.status_code == httplib.CREATED:
+        ctx.logger.info('Acquired host: {0}'.format(host['host']))
         ctx.instance.runtime_properties['ip'] = host['host']
-        key_path = _save_keyfile(host['auth']['keyfile'], host['host_id'])
-        agent = {'user': host['auth']['username'],
-                 'key': key_path}
-        ctx.instance.runtime_properties['cloudify_agent'] = agent
+        key_content = host['auth'].get('keyfile')
+        if key_content:
+            key_path = _save_keyfile(key_content, host['host_id'])
+            ctx.instance.runtime_properties['key'] = key_path
+        else:
+            ctx.instance.runtime_properties['password'] = \
+                host['auth'].get('password')
+        ctx.instance.runtime_properties['user'] = host['auth']['username']
+        ctx.instance.runtime_properties['port'] = host['port']
         ctx.instance.runtime_properties['host'] = host
+    elif response.status_code != httplib.INTERNAL_SERVER_ERROR:
+        raise NonRecoverableError(response.json()['error'])
     else:
-        raise NonRecoverableError(response.json())
+        raise NonRecoverableError('Internal server error')
 
 
 @operation
 def release(service_url, **kwargs):
-    ctx.logger.info("Release host")
+    ctx.logger.info('Release host')
     host_id = ctx.instance.runtime_properties['host']['host_id']
     response = requests.delete('{0}/hosts/{1}'.format(service_url, host_id))
-    ctx.logger.info("Response received: {0}".format(str(response)))
+    ctx.logger.debug('Response received: {0}'.format(str(response)))
     if not response.ok:
         ctx.logger.warning(response.text)
-    key_file = ctx.instance.runtime_properties['cloudify_agent'].get('key')
+    key_file = ctx.instance.runtime_properties.get('key')
     if key_file and os.path.exists(key_file):
         os.unlink(key_file)
 
 
 def _save_keyfile(key_content, host_id):
     ctx.logger.info("Save key")
-    key_path = "~/.ssh/key_{0}".format(host_id)
-    key_path = os.path.expanduser(key_path)
+    key_path = os.path.expanduser("~/.ssh/key_{0}".format(host_id))
     with open(key_path, 'w') as f:
         f.write(key_content)
+    os.chmod(key_path, stat.S_IRUSR | stat.S_IWUSR)
     return key_path
