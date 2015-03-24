a05ac3bad314f0149624c753c9e74b0cb6a516e4
Anna Szpoton <anna.szpoton@codilime.com>
2015-03-24 15:56:58 +0100

    CFY-2295 Extend error verbosity

--- cloudify_hostpool_plugin/tasks.py
+++ cloudify_hostpool_plugin/tasks.py
@@ -12,7 +12,6 @@
 # * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # * See the License for the specific language governing permissions and
 # * limitations under the License.
-import httplib
 import os
 import requests
 import stat
@@ -26,9 +25,9 @@ from cloudify.decorators import operation
 def acquire(service_url, **kwargs):
     ctx.logger.info('Acquire host')
     response = requests.post('{0}/hosts'.format(service_url))
-    host = response.json()
-    ctx.logger.debug('Response received: {0}'.format(str(host)))
-    if response.status_code == httplib.CREATED:
+    ctx.logger.debug('Response received: {0}'.format(response.text))
+    if response.status_code == requests.codes.CREATED:
+        host = response.json()
         ctx.logger.info('Acquired host: {0}'.format(host['host']))
         ctx.instance.runtime_properties['ip'] = host['host']
         key_content = host['auth'].get('keyfile')
@@ -41,11 +40,8 @@ def acquire(service_url, **kwargs):
         ctx.instance.runtime_properties['user'] = host['auth']['username']
         ctx.instance.runtime_properties['port'] = host['port']
         ctx.instance.runtime_properties['host'] = host
-    elif response.status_code != httplib.INTERNAL_SERVER_ERROR:
-        raise NonRecoverableError(
-            response.json().get('error', response.status_code))
     else:
-        raise NonRecoverableError('Internal server error')
+        _handle_error(response)
 
 
 @operation
@@ -55,8 +51,7 @@ def release(service_url, **kwargs):
     response = requests.delete('{0}/hosts/{1}'.format(service_url, host_id))
     ctx.logger.debug('Response received: {0}'.format(str(response)))
     if not response.ok:
-        raise NonRecoverableError(
-            response.json().get('error', response.status_code))
+        _handle_error(response)
     key_file = ctx.instance.runtime_properties.get('key')
     if key_file and os.path.exists(key_file):
         os.unlink(key_file)
@@ -64,8 +59,17 @@ def release(service_url, **kwargs):
 
 def _save_keyfile(key_content, host_id):
     ctx.logger.info('Save key')
-    key_path = os.path.expanduser("~/.ssh/key_{0}".format(host_id))
+    key_path = os.path.expanduser('~/.ssh/key_{0}'.format(host_id))
     with open(key_path, 'w') as f:
         f.write(key_content)
     os.chmod(key_path, stat.S_IRUSR | stat.S_IWUSR)
     return key_path
+
+
+def _handle_error(response):
+    try:
+        reason = response.json().get('error', response.reason)
+    except ValueError:
+        reason = response.reason
+    raise NonRecoverableError(
+        'Error: {0} Reason: {1}'.format(response.status_code, reason))
