60f6de13ac9b6304b9caac3b99f58a54ac172ef8
opencm <limor@gigaspaces.com>
2015-04-01 12:47:08 +0300

    Bump version to 1.2m8

--- setup.py
+++ setup.py
@@ -17,13 +17,13 @@ from setuptools import setup
 
 setup(
     name='cloudify-host-pool-plugin',
-    version='1.0',
+    version='1.2a8',
     license='LICENSE',
     packages=['cloudify_hostpool_plugin'],
     description='A Cloudify plugin enabling hosts acquisition '
                 'via cloudify-host-pool-service',
     install_requires=[
-        'cloudify-plugins-common>=3.2a6',
+        'cloudify-plugins-common>=3.2a8',
         'requests'
     ]
 )
