d63017041173c6b4bc7c28326ac6a37918019d7d
Joshua Cornutt <jcornutt@gmail.com>
2016-02-15 21:04:36 -0500

    Fixing Windows support, adding REST-only service comm
    Adding port specifier for Windows hosts
    
    Adding Windows Host Support
    
    Adding Windows host support, fixing tasks
    
    Added proper agent inputs for Windows hosts
    
    Cleaning up tasks and updating Windows support
    
    Cleaning up tasks and updating Windows support
    
    Setting Cosmo source
    
    Adding Windows host support, fixing tasks
    
    Added proper agent inputs for Windows hosts
    
    Cleaning up tasks and updating Windows support
    
    Adding Windows host support, fixing tasks
    
    Added proper agent inputs for Windows hosts
    
    Cleaning up tasks and updating Windows support
    
    Cleaning up tasks and updating Windows support
    
    Setting Cosmo source
    
    Adding Windows host support, fixing tasks
    
    Adding port specifier for Windows hosts
    
    Added proper agent inputs for Windows hosts
    
    Cleaning up tasks and updating Windows support
    
    Fixing tests
    
    Fixing tests
    
    Updating how keys are handled
    
    Creating Linux/Windows node types
    
    Creating Linux/Windows node types
    
    Creating Linux/Windows node types
    
    debugging
    
    Syncing with service
    
    Preparing for merge
    
    Fixing test

--- cloudify_hostpool_plugin/tasks.py
+++ cloudify_hostpool_plugin/tasks.py
@@ -1,18 +1,17 @@
 # #######
-# Copyright (c) 2015 GigaSpaces Technologies Ltd. All rights reserved
+# Copyright (c) 2016 GigaSpaces Technologies Ltd. All rights reserved
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#        http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# * See the License for the specific language governing permissions and
-# * limitations under the License.
-
+#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    * See the License for the specific language governing permissions and
+#    * limitations under the License.
 '''
     tasks
     ~~~~~
@@ -20,9 +19,12 @@
 '''
 
 import os
+import httplib
 import requests
-from requests.exceptions import RequestException, Timeout
 import stat
+
+from urlparse import urlparse
+from requests.exceptions import RequestException, Timeout
 
 from cloudify import ctx
 from cloudify.exceptions import NonRecoverableError, RecoverableError
@@ -30,58 +32,73 @@ from cloudify.decorators import operation
 
 RUNTIME_PROPERTIES_KEYS = [
     'ip', 'user', 'port',
-    'password', 'key', 'host_id',
-    'public_address'
+    'password', 'key', 'host_id'
 ]
 
 
 @operation
-def acquire(service_url, **_):
-    '''Acquire a host for the user'''
-    ctx.logger.info('Acquiring host')
+def acquire(service_url, requested_os='linux', **_):
+    '''Allocate a host for the user from the pool'''
+    # Format the OS request
+    if not isinstance(requested_os, basestring):
+        raise NonRecoverableError('Requested OS must be a string')
+    requested_os = requested_os.lower()
+
+    ctx.logger.info('Allocating host')
+    ctx.logger.info('Calling POST "{0}", data="{1}"'.format(
+        '{0}/host/allocate'.format(service_url), requested_os))
     try:
-        response = requests.post('{0}/hosts'.format(service_url))
-        ctx.logger.debug('Response received: {0}'.format(response.text))
+        response = requests.post(
+            '{0}/host/allocate'.format(service_url),
+            json={'os': requested_os})
+        ctx.logger.debug('Response received: {0}'.format(response.json()))
     except Timeout:
         # Catch ConnectTimeout & ReadTimeout errors
-        raise RecoverableError('Timeout acquiring host')
-    except RequestException:
-        # Catastrophic network error
-        raise NonRecoverableError('Fatal network error acquiring host')
+        raise RecoverableError('Timeout allocating host')
+    except RequestException as exc:
+        # Network error
+        raise RecoverableError(
+            'Network error allocating host: {0}'.format(exc))
 
     # Handle bad responses
-    if response.status_code != requests.codes.CREATED:
+    if response.status_code != httplib.OK:
         _handle_error(response)
 
     host = response.json()
-    ctx.logger.info('Acquired host: {0}'.format(host['host']))
+    ctx.logger.info('Allocated host: {0}'.format(host))
 
-    key_content = host.get('auth', dict()).get('keyfile')
+    key_content = host.get('credentials', dict()).get('key')
     key_path = None
+    svc_urlparse = urlparse(service_url)
     if key_content:
-        key_path = _save_keyfile(key_content, host['host_id'])
+        key_path = _save_keyfile(
+            key_content,
+            '{0}_{1}_{2}'.format(
+                svc_urlparse.hostname,
+                svc_urlparse.port,
+                host['id']))
 
     _set_runtime_properties(host, key_path)
 
 
 @operation
 def release(service_url, **_):
-    '''Release a host from use by the user'''
-    ctx.logger.info('Releasing host')
+    '''Deallocate a host from the user to the pool'''
+    ctx.logger.info('Deallocating host')
     host_id = ctx.instance.runtime_properties['host_id']
     try:
-        response = requests.delete('{0}/hosts/{1}'
+        response = requests.delete('{0}/host/{1}/deallocate'
                                    .format(service_url, host_id))
         ctx.logger.debug('Response received: {0}'.format(response))
     except Timeout:
         # Catch ConnectTimeout & ReadTimeout errors
-        raise RecoverableError('Timeout releasing host')
+        raise RecoverableError('Timeout deallocating host')
     except RequestException:
         # Catastrophic network error
-        raise NonRecoverableError('Fatal network error releasing host')
+        raise NonRecoverableError('Fatal network error deallocating host')
 
     # Handle bad responses
-    if not response.ok:
+    if response.status_code != httplib.NO_CONTENT:
         _handle_error(response)
 
     key_file = ctx.instance.runtime_properties['key']
@@ -91,9 +108,10 @@ def release(service_url, **_):
     _delete_runtime_properties()
 
 
-def _save_keyfile(key_content, host_id):
+def _save_keyfile(key_content, key_id):
     '''Write host SSH key to ~/.ssh'''
-    key_path = os.path.expanduser('~/.ssh/key_{0}'.format(host_id))
+    key_path = os.path.expanduser('~/.ssh/key_{0}'.format(key_id))
+    ctx.logger.debug('Saving key contents to {0}'.format(key_path))
     with open(key_path, 'w') as f_key:
         f_key.write(key_content)
     os.chmod(key_path, stat.S_IRUSR | stat.S_IWUSR)
@@ -112,16 +130,16 @@ def _handle_error(response):
 
 def _set_runtime_properties(host, key_path):
     '''Sets runtime properties for the acquired host'''
-    ctx.instance.runtime_properties['host_id'] = host.get('host_id')
-    ctx.instance.runtime_properties['ip'] = host.get('host')
-    ctx.instance.runtime_properties['port'] = host.get('port')
+    ctx.instance.runtime_properties['host_id'] = host['id']
+    ctx.instance.runtime_properties['ip'] = \
+        host.get('endpoint', dict()).get('ip')
+    ctx.instance.runtime_properties['port'] = \
+        host.get('endpoint', dict()).get('port')
     ctx.instance.runtime_properties['user'] = \
-        host.get('auth', dict()).get('username')
+        host.get('credentials', dict()).get('username')
     ctx.instance.runtime_properties['password'] = \
-        host.get('auth', dict()).get('password')
+        host.get('credentials', dict()).get('password')
     ctx.instance.runtime_properties['key'] = key_path
-    ctx.instance.runtime_properties['public_address'] = \
-        host.get('public_address')
 
 
 def _delete_runtime_properties():
