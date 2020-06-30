d82a2345383ba73ccb19ff27ae5279003e3b8dab
AdarShaked <adar@cloudify.co>
2020-06-30 14:58:24 +0300

    added logic to handle fabric plugin upload

--- .circleci/test_examples.py
+++ .circleci/test_examples.py
@@ -27,51 +27,11 @@ from ecosystem_tests.dorkl import (
     prepare_test,
     cloudify_exec
 )
-
-OS_VERSION = '3.2.16'
-OS_WAGON = 'https://github.com/cloudify-cosmo/cloudify-openstack-plugin/' \
-           'releases/download/{v}/cloudify_openstack_plugin-{v}-' \
-           'centos-Core-py27.py36-none-linux_x86_64.wgn'.format(v=OS_VERSION)
-OS_PLUGIN = 'https://github.com/cloudify-cosmo/cloudify-openstack-plugin/' \
-            'releases/download/{v}/plugin.yaml'.format(v=OS_VERSION)
-'''Temporary until all the plugins in the bundle will 
-released with py2py3 wagons'''
-UT_VERSION = '1.23.5'
-UT_WAGON = 'https://github.com/cloudify-incubator/cloudify-utilities-plugin/' \
-           'releases/download/{v}/cloudify_utilities_plugin-{v}-centos' \
-           '-Core-py27.py36-none-linux_x86_64.wgn'.format(v=UT_VERSION)
-UT_PLUGIN = 'https://github.com/cloudify-incubator/cloudify-utilities-' \
-            'plugin/releases/download/{v}/plugin.yaml'.format(v=UT_VERSION)
-FAB_VERSION = '2.0.3'
-FAB_WAGON = 'https://github.com/cloudify-cosmo/cloudify-fabric-plugin/' \
-            'releases/download/{v}/cloudify_fabric_plugin-{v}-centos-' \
-            'Core-py27.py36-none-linux_x86_64.wgn'.format(v=FAB_VERSION)
-FAB_PLUGIN = 'https://github.com/cloudify-cosmo/cloudify-fabric-plugin/' \
-             'releases/download/{v}/plugin.yaml'.format(v=FAB_VERSION)
-PLUGINS_TO_UPLOAD = [(OS_WAGON, OS_PLUGIN), (UT_WAGON, UT_PLUGIN),
-                     (FAB_WAGON, FAB_PLUGIN)]
+from . import (get_plugins_to_upload, SECRETS_TO_CREATE)
 
 
-SECRETS_TO_CREATE = {
-    'openstack_username': False,
-    'openstack_password': False,
-    'openstack_tenant_name': False,
-    'openstack_auth_url': False,
-    'openstack_region': False,
-    'openstack_region_name': False,
-    'openstack_external_network': False,
-    'openstack_project_id': False,
-    'openstack_project_name': False,
-    'openstack_project_domain_id': False,
-    'openstack_user_domain_name': False,
-    'openstack_project_domain_name': False,
-    'base_image_id': False,
-    'base_flavor_id': False,
-}
-
-prepare_test(plugins=PLUGINS_TO_UPLOAD, secrets=SECRETS_TO_CREATE,
+prepare_test(plugins=get_plugins_to_upload(), secrets=SECRETS_TO_CREATE,
              execute_bundle_upload=False)
-
 
 infra_blueprint = \
     'examples/service/examples/' \
